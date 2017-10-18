dcde13291f7f5c3d23e709461898e13f9d2b9a45
learn <learn@learn03.local>
2017-10-18 11:40:28 -0700

    Created grid by using Jquery and for loops

---
+++ control.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  //loop appends 10 table rows with ids into the table
+  for(var i = 0; i < 10; i++) {
+    $("table").append("<tr id=" + i + "></tr>");
+    //loop appends 10 table datas with ids into the table rows
+    for(var x = 0; x < 10; x++) {
+    $("#" + i).append("<td id= td" + x + ">0</td>");
+    }
+  }
+
+
+});
