3e00ba0385c1c5c6c1fd263d3f6cec1e241c92d1
learn <learn@learn03.local>
2017-10-19 10:38:57 -0700

    added check for ships in array and display hit or miss, and hit counter

--- control.js
+++ control.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
   // set our torpedoe values globally
   var torpedoe = 0;
   var torpLeft = 25;
+  var hitsCount = 0;
+
   //created a global array called board with 10 empty arrays
   // with 10 spots which gives you the 100 squares of board game
-
   var board = [[], [], [], [], [], [], [], [], [], []];
+
   //created a global constant variable called SHIP with a value of one.
   const SHIP = 1;
   var cords = [];
@@ -22,18 +24,16 @@ $(document).ready(function() {
   }
   console.log(board);
 
-  //loop appends 10 table rows with ids into the table
+  //loop appends 10 table rows with ids into the table (making the grid)
   for(var i = 0; i < 10; i++) {
     $("table").append("<tr id=" + i + "></tr>");
     //loop appends 10 table datas with ids into the table rows
     for(var x = 0; x < 10; x++) {
-    $("#" + i).append("<td id= td" + x + ">0</td>");
+    $("#" + i).append("<td id= td" + x + "></td>");
     }
   }
-  // onclick changes color, counts torps and
+  // onclick changes color, counts torps and displays "HIT" or "MISS"
   $("td").on("click", (function(){
-    $(this).addClass("torpedoed");
-    $(this).off("click");
 
     // using the short hand -- and ++ for minus and adding to our torpedoe values
     torpLeft--;
@@ -41,6 +41,24 @@ $(document).ready(function() {
     // displaying torpedoes used and remaining floated to the left of table
     $(".torpCount").text(torpedoe);
     $(".torpRemain").text(torpLeft);
+
+    // Finding ids for board array
+    var rowID = $(this).parent().attr("id");
+    var tdID = $(this).attr("id");
+    tdID = (tdID[tdID.length - 1])
+    //checks board array for SHIP
+      if (board[rowID][tdID] == 1) {
+        $(this).text("Hit!");
+        $(this).addClass("torpedoed");
+        $(this).off("click");
+        hitsCount++;
+      } else {
+        $(this).text("Miss!");
+        $(this).addClass("missed");
+        $(this).off("click");
+      }
+
+
 
 
   }));
