059f98ced601103a9a2be8bd4a2d136a74252244
learn <learn@learn03.local>
2017-10-19 17:18:04 -0700

    game now end with enough hits on ships

--- control.js
+++ control.js
@@ -1,28 +1,19 @@
 $(document).ready(function() {
   // set our torpedoe values globally
   var torpedoe = 0;
-  var torpLeft = 25;
+  var torpLeft = 5;
   var hitsCount = 0;
-
+  var isGameOver = false;
+  var shipNum = 2;
+  //created a global constant variable called SHIP with a value of one.
+  const SHIP = 1;
   //created a global array called board with 10 empty arrays
   // with 10 spots which gives you the 100 squares of board game
   var board = [[], [], [], [], [], [], [], [], [], []];
+  var randRow;
+  var randCol;
 
-  //created a global constant variable called SHIP with a value of one.
-  const SHIP = 1;
-  var cords = [];
-
-  //function to assign 5 random locations for ships
-  for(var y = 0; y < 5; y++){
-    function randNums(){
-      var randRow = Math.floor(Math.random() * 10) + 0;
-      var randCol = Math.floor(Math.random() * 10) + 0;
-      return [randRow, randCol]
-    }
-    cords = randNums();
-    board[cords[0]][cords[1]] = SHIP;
-  }
-  console.log(board);
+  // CREATE BOARD -----------------------------------------------------------
 
   //loop appends 10 table rows with ids into the table (making the grid)
   for(var i = 0; i < 10; i++) {
@@ -32,6 +23,40 @@ $(document).ready(function() {
     $("#" + i).append("<td id= td" + x + "></td>");
     }
   }
+
+  // PLACE SHIPS -----------------------------------------------------------
+
+  //function to assign random locations for ships
+  function randNums(){
+    var randRow = Math.floor(Math.random() * 10) + 0;
+    var randCol = Math.floor(Math.random() * 10) + 0;
+    return [randRow, randCol]
+  }
+
+
+    var hiddenCords = []
+  // for loop runs randNum to get location of ships
+  for(let y = 0; y < shipNum; y++){
+    var cords = randNums();
+    // save coordinates
+
+    var newCord = randNums();
+    cords.push(newCord);
+
+    // add class of hiddenShips to each square with a ship
+
+    $('tr#'+cords[0]).find('#td'+cords[1]).addClass("hiddenShips");
+
+    // update board array with ship location
+    board[cords[0]][cords[1]] = SHIP;
+    // hiddenCords = [cords[0], cords[1]];
+  }
+  console.log("newCord: " + newCord);
+  console.log("cords: " + cords);
+  console.log("hiddenCords: " + hiddenCords);
+  console.log(board);
+
+
   // onclick changes color, counts torps and displays "HIT" or "MISS"
   $("td").on("click", (function(){
 
@@ -57,6 +82,21 @@ $(document).ready(function() {
         $(this).addClass("missed");
         $(this).off("click");
       }
+
+      if (torpLeft == 0) {
+        $("#winDisplay").text("You're out of torpedoes!");
+        $("td").off("click");
+      }
+
+      if (hitsCount == shipNum) {
+        $("#winDisplay").text("You Won!");
+        $("td").off("click");
+        isGameOver = true;
+      }
+
+      // if (isGameOver) {
+      //
+      // }
 
 
 
